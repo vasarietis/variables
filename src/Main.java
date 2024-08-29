53161b23519c55b6de4999203cd09fc438db10ac
vasarietis <j.vasarietis@gmail.com>
2024-08-29 16:04:13 +0300

    adding files

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main (String[] args){
+
+        int x = 123; //initialization
+
+        //System.out.println(x);  vai
+
+        System.out.println("My number is: "+x);
+
+        //ja ir ļoti liels skaitlis, tad int vietā jāraksta long un ciparu beigās jāliek L:
+
+
+    }
+}
